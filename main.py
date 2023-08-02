48000a6ed9843c747e10e1ba0ccc4571a7f34d71
SimaR266 <121790287+SimaR266@users.noreply.github.com>
2023-08-02 09:38:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
+
 import logging
+import os
 from flask import Flask
 from telegram.ext import Updater, CommandHandler
-import os
 
-# Flask uygulamasını başlatın
 app = Flask(__name__)
 
-# Botun başlamasını sağlayan komut işleyicisi
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Merhaba!")
 
-# Botun size "merhaba" demesine izin veren komut işleyicisi
 def hello(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Selam!")
 
 def main():
-    # Telegram botunuzun tokenini burada belirtin
-    token = '5761864354:AAGbpgsIe3Nyp-FzRDylpkzN3lMXCS283SQ'
+    token = 'YOUR_TELEGRAM_BOT_TOKEN'
     updater = Updater(token=token, use_context=True)
-
     dispatcher = updater.dispatcher
-    
-    # "/start" komutuna başlama işlevini atayın
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
-    
-    # "/hello" komutuna "hello" işlevini atayın
     hello_handler = CommandHandler('hello', hello)
     dispatcher.add_handler(hello_handler)
 
-    # Botu başlatın
-    port = int(os.environ.get("PORT", 10000))  # Portu alın veya varsayılan olarak 5000 kullanın
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)  # Günlüğü yapılandırın
+    logging.basicConfig(level=logging.INFO)
     main()
