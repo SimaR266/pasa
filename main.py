d309fb9bbb24aaba05b7786382151b3c11751745
SimaR266 <121790287+SimaR266@users.noreply.github.com>
2023-08-02 09:40:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
     hello_handler = CommandHandler('hello', hello)
     dispatcher.add_handler(hello_handler)
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
