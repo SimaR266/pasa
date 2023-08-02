4356ece2769ec6b93285fc0dbe03e71f927e0f83
SimaR266 <121790287+SimaR266@users.noreply.github.com>
2023-08-02 08:29:37 +0200

    Add files via upload
    Main ğy

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
 
 import logging
-from flask import Flask, request
-import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler
 
-# Flask uygulamasını başlatın
-app = Flask(__name__)
-
-# Telegram bot token'ınızı buraya girin
-BOT_TOKEN = "5761864354:AAGbpgsIe3Nyp-FzRDylpkzN3lMXCS283SQ"
-updater = Updater(BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-# /start komutunu işleyen fonksiyon
+# Botun başlamasını sağlayan komut işleyicisi
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Bot açık")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Merhaba!")
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+# Botun size "merhaba" demesine izin veren komut işleyicisi
+def hello(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Selam!")
 
-# Mesajları işleyen fonksiyon
-def handle_message(update, context):
-    message_text = update.message.text
-    if message_text.lower() == "merhaba":
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Selam")
+def main():
+    # Telegram botunuzun tokenini burada belirtin
+    token = '5761864354:AAGbpgsIe3Nyp-FzRDylpkzN3lMXCS283SQ'
+    updater = Updater(token=token, use_context=True)
 
-message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
-dispatcher.add_handler(message_handler)
+    dispatcher = updater.dispatcher
+    
+    # "/start" komutuna başlama işlevini atayın
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    
+    # "/hello" komutuna "hello" işlevini atayın
+    hello_handler = CommandHandler('hello', hello)
+    dispatcher.add_handler(hello_handler)
 
-# Flask HTTP webhook'u etkinleştirme
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), updater.bot)
-    dispatcher.process_update(update)
-    return 'ok'
+    # Botu çalıştırın
+    updater.start_polling()
+
+    # Botun sürekli çalışmasını sağlayın
+    updater.idle()
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    # Flask uygulamanızı gunicorn ile çalıştırın
-    app.run(host='0.0.0.0', port=8080)
+    main()
