97b89b67bfdc97ff2283a9c71f1a847b6390bc71
SimaR266 <121790287+SimaR266@users.noreply.github.com>
2023-08-06 12:23:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,23 @@
 from flask import Flask, request
-import telegram
+from telegram.ext import Updater, CommandHandler
 
 app = Flask(__name__)
-bot = telegram.Bot(token='5761864354:AAGbpgsIe3Nyp-FzRDylpkzN3lMXCS283SQ')
+updater = Updater(token='5761864354:AAGbpgsIe3Nyp-FzRDylpkzN3lMXCS283SQ', use_context=True)
+dispatcher = updater.dispatcher
 
 @app.route('https://api.render.com/deploy/srv-cj4af02ip7vuask3tcfg?key=qiWPDsXBCuc', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        chat_id = update.message.chat_id
-        text = update.message.text
+        update = request.get_json()
+        text = update['message']['text']
+        chat_id = update['message']['chat']['id']
 
-        # Eğer gelen mesaj "merhaba" ise "selam" cevabını veriyoruz
         if text.lower() == 'merhaba':
-            bot.sendMessage(chat_id=chat_id, text='Selam!')
+            context.bot.send_message(chat_id=chat_id, text='Selam!')
 
         return 'ok'
 
 if __name__ == '__main__':
-    # Flask uygulamasını çalıştırırken webhook URL'sini uygulamanızın URL'si olarak ayarlayın
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path='YOUR_WEBHOOK_URL', webhook_url='https://YOUR_APP_URL/YOUR_WEBHOOK_URL')
+    updater.bot.set_webhook('https://YOUR_APP_URL/YOUR_WEBHOOK_URL')
     app.run(threaded=True)
