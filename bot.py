8df2b68fcd1bef24a0facad5da0e3ad625041262
SimaR266 <121790287+SimaR266@users.noreply.github.com>
2023-08-06 12:33:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 updater = Updater(token='5761864354:AAGbpgsIe3Nyp-FzRDylpkzN3lMXCS283SQ', use_context=True)
 dispatcher = updater.dispatcher
 
-@app.route('https://api.render.com/deploy/srv-cj4af02ip7vuask3tcfg?key=qiWPDsXBCuc', methods=['POST'])
+@app.route('/YOUR_WEBHOOK_URL', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         update = request.get_json()
@@ -18,6 +18,6 @@ def webhook():
         return 'ok'
 
 if __name__ == '__main__':
-    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path='YOUR_WEBHOOK_URL', webhook_url='https://YOUR_APP_URL/YOUR_WEBHOOK_URL')
-    updater.bot.set_webhook('https://YOUR_APP_URL/YOUR_WEBHOOK_URL')
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path='YOUR_WEBHOOK_URL', webhook_url='https://api.render.com/deploy/srv-cj4af02ip7vuask3tcfg?key=qiWPDsXBCuc')
+    updater.bot.set_webhook('https://api.render.com/deploy/srv-cj4af02ip7vuask3tcfg?key=qiWPDsXBCuc')
     app.run(threaded=True)
